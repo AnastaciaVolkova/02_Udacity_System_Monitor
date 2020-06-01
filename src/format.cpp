169edfa41f94b4a8b79180544efa49611f96e271
AnastaciaVolkova <polarbolide@gmail.com>
2020-06-01 11:36:19 +0200

    feat: Realize Format::ElapsedTime

--- src/format.cpp
+++ src/format.cpp
@@ -1,11 +1,23 @@
+#include <iomanip>
+#include <sstream>
 #include <string>
 
 #include "format.h"
 
 using std::string;
 
-// TODO: Complete this helper function
+// DONE: Complete this helper function
 // INPUT: Long int measuring seconds
 // OUTPUT: HH:MM:SS
-// REMOVE: [[maybe_unused]] once you define the function
-string Format::ElapsedTime(long seconds[[maybe_unused]]) { return string(); }
+string Format::ElapsedTime(long seconds) {
+  // s + 60*min + 3600*h
+  long hours = seconds / 3600;
+  hours = hours % 24;  // Get rid of days.
+  seconds = seconds % 3600;
+  long minutes = seconds / 60;
+  seconds = seconds % 60;
+  std::ostringstream oss;
+  oss << std::setfill('0') << std::setw(2) << std::to_string(hours) << ":"
+      << std::to_string(minutes) << ":" << std::to_string(seconds);
+  return oss.str();
+}
