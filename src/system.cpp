628be23fed51b0bf5dd3314d70cf82078a387152
AnastaciaVolkova <polarbolide@gmail.com>
2020-06-02 02:00:16 +0200

    feat: Use LinuxParser functions in System class

--- src/system.cpp
+++ src/system.cpp
@@ -28,54 +28,18 @@ Processor& System::Cpu() { return cpu_; }
 
 // DONE: Return a container composed of the system's processes
 vector<Process>& System::Processes() {
-  DIR* dirp = opendir("/proc/");
-  struct dirent* dp;
-
-  std::regex pat("[0-9]+");
-  std::smatch sm;
-
-  while ((dp = readdir(dirp)) != NULL) {
-    string s(dp->d_name);
-    if (std::regex_match(s, sm, pat))
-      processes_.push_back(Process(std::stoi(string(sm[0].str()))));
-  }
-  return processes_;
+  vector<int> pids = LinuxParser::Pids();
+  for (auto p : pids) processes_.push_back(Process(p));
 }
 
 // DONE: Return the system's kernel identifier (string)
-std::string System::Kernel() {
-  ifstream ifs("/proc/version");
-  string line;
-  getline(ifs, line);
-  ifs.close();
-  istringstream iss(line);
-  vector<string> tokens(istream_iterator<string>{iss},
-                        istream_iterator<string>());
-
-  return tokens[2];
-}
+std::string System::Kernel() { return LinuxParser::Kernel(); }
 
 // DONE: Return the system's memory utilization
 float System::MemoryUtilization() { return LinuxParser::MemoryUtilization(); }
 
 // DONE: Return the operating system name
-std::string System::OperatingSystem() {
-  ifstream ifs("/etc/os-release");
-  string key = "", val = "";
-  // Find string with operating system name.
-  do {
-    string line;
-    getline(ifs, line);
-    istringstream iss(line);
-    getline(iss, key, '=');
-    getline(iss, val, '=');
-  } while (key != "PRETTY_NAME");
-  ifs.close();
-  // Trim quotes.
-  if (val[0] == '\"') val.erase(0, 1);
-  if (val[val.size() - 1] == '\"') val.erase(val.size() - 1, 1);
-  return val;
-}
+std::string System::OperatingSystem() { return LinuxParser::OperatingSystem(); }
 
 // DONE: Return the number of processes actively running on the system
 int System::RunningProcesses() { return LinuxParser::RunningProcesses(); }
