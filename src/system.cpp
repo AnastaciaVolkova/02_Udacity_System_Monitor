2f58a21fee901e77725b995a0feed50d9d6e5fe8
AnastaciaVolkova <polarbolide@gmail.com>
2020-06-02 02:01:56 +0200

    fix: Add return to function System::Processes

--- src/system.cpp
+++ src/system.cpp
@@ -30,6 +30,7 @@ Processor& System::Cpu() { return cpu_; }
 vector<Process>& System::Processes() {
   vector<int> pids = LinuxParser::Pids();
   for (auto p : pids) processes_.push_back(Process(p));
+  return processes_;
 }
 
 // DONE: Return the system's kernel identifier (string)
